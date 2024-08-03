53d104d58999fae821da35f0c18063a5ff29adaa
Alex LaFroscia <alex@lafroscia.com>
2024-08-03 13:11:56 -0400

    depend on `compiled-package`

--- app/src/lib/Counter.svelte
+++ app/src/lib/Counter.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-  let count: number = 0
+  import { add } from "compiled-package";
+
+  let count: number = 0;
+
   const increment = () => {
-    count += 1
-  }
+    count = add(count, 1);
+  };
 </script>
 
 <button on:click={increment}>
